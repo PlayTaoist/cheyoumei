3ead84342acb07a788006d942f095f015968ed5a
花花道长 <liuren-mail@163.com>
2016-11-10 17:26:31 +0800

    Add files via upload

---
+++ src/main/webapp/js/controlserver/view/right/MainView.js
@@ -0,0 +1,7 @@
+Ext.define('controlserver.view.right.MainView', {
+			extend : 'Ext.panel.Panel',
+			xtype : 'mainView',
+			title : '状态监控',
+			icon : 'img/main_left/monitor_lightning.png',
+			closable : false
+		})
